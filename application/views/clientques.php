537dc4d7f8abaff3e88174590dcf2d35c11390d2
Daryl Vi <darylvi@DV-MBP.local>
2015-05-23 10:20:26 +0800

    modified layout, changed to bootstrap

--- application/views/clientques.php
+++ application/views/clientques.php
@@ -1,24 +1,65 @@
 <?php require_once 'header.php'; ?>
-<body class="clientquess">
-<div class="clientfield">
 
-		<div class="quetitski">QUEUEING SYSTEM PROGRESS</div>
-        <div class="listaf">
-        		
-                <div class="listanaf">
-                        <div class="listanumber">999</div>
-                        <div class="listaname">MARIA LEONORA THERESA</div>
-                        <div class="listastatus">APPOINTEMENT</div>
-                </div> <!-----listanaf close ---->     
-        </div> <!----listaf close--->
+<div class="container-fluid">
+			
+            	<div class="row">
+                	<div class="que_h_cont col-lg-12">
+                		<center><div class="que_h">QUEUEING SYSTEM PROGRESS</div></center>
+                    </div>
+                </div>
+			
+                    <div class="row">
+                            <div class="table_height col-lg-12">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped listaf"></table>
+                                    </div>
+                            </div>
+                    </div>
+                    
+                      <div class="row">
+                            <div class="buts_cont col-lg-12">
+                            	<div class="nav nav-tabs hide_que" role="navigation" aria-label="...">
+                                      <div class="btn-group" role="group">
+                                        <button type="button" id ="allque" class="btn btn-primary try">All Que</button>
+                                      </div>
+                                      <div class="btn-group " role="group">
+                                        <button type="button" id ="forrepair_que" class="btn btn-primary try">For Repair</button>
+                                      </div>
+                                      <div class="btn-group" role="group">
+                                        <button type="button" id ="releasing_que" class="btn btn-primary try">Releasing</button>
+                                      </div>
+                                       <div class="btn-group" role="group">
+                                        <button type="button" id ="appointment_que" class="btn btn-primary try">Appointment</button>
+                                      </div>
+                                       <div class="btn-group" role="group">
+                                        <button type="button" id ="inquiry_que" class="btn btn-primary try">Inquiry</button>
+                                      </div>
+                                      	<input type="hidden" class="selected_que" value="ALL">
+                                 </div>
+                            </div>
+                 	  </div><!---row-->
+                      
+                      			
+                                	<div class="row">
+                                    
+                                    
+                                    
+                                    </div>
+   
+   
+</div> <!---container-fluid-->
+   
 
 
-					<div class="refreshbuts"></div>
+			<!--		<div class="refreshbuts"></div>
 
 					<div class="pmclogso"></div>
 
 
 
 </div> <!------ clientfield closure --->
+
+
+
 
 <?php require_once 'footer.php'; ?>
