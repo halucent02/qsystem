5a9402f684f87a9fb07b1cc78fc6336abdea4a1f
Daryl Vi <darylvi@DV-MBP.local>
2015-05-25 11:54:52 +0800

    modified table style,ln 14, modify buttons design from ln19 to ln38

--- application/views/clientques.php
+++ application/views/clientques.php
@@ -9,55 +9,39 @@
                 </div>
 			
                     <div class="row">
-                            <div class="table_height col-lg-12">
+                            <div class="table_height col-lg-12 col-xs-12">
                                     <div class="table-responsive">
-                                        <table class="table table-striped listaf"></table>
+                                        <table class=" table table-striped table-bordered table-condensed table-hover listaf"></table>
                                     </div>
                             </div>
                     </div>
-                    
+                    <hr>
                       <div class="row">
-                            <div class="buts_cont col-lg-12">
-                            	<div class="nav nav-tabs hide_que" role="navigation" aria-label="...">
-                                      <div class="btn-group" role="group">
-                                        <button type="button" id ="allque" class="btn btn-primary try">All Que</button>
-                                      </div>
-                                      <div class="btn-group " role="group">
-                                        <button type="button" id ="forrepair_que" class="btn btn-primary try">For Repair</button>
-                                      </div>
-                                      <div class="btn-group" role="group">
-                                        <button type="button" id ="releasing_que" class="btn btn-primary try">Releasing</button>
-                                      </div>
-                                       <div class="btn-group" role="group">
-                                        <button type="button" id ="appointment_que" class="btn btn-primary try">Appointment</button>
-                                      </div>
-                                       <div class="btn-group" role="group">
-                                        <button type="button" id ="inquiry_que" class="btn btn-primary try">Inquiry</button>
-                                      </div>
-                                      	<input type="hidden" class="selected_que" value="ALL">
-                                 </div>
-                            </div>
-                 	  </div><!---row-->
-                      
-                      			
-                                	<div class="row">
-                                    
-                                    
-                                    
-                                    </div>
-   
-   
+                     
+                             <div class="btn-group col-lg-2 col-md-12 col-xs-12" role="group">
+                                        <button type="button" id ="allque" class="btn btn-primary testings">All Queue</button>
+                             </div>
+                             
+                              <div class="btn-group col-lg-2 col-md-12 col-xs-12"  role="group">
+                                        <button type="button" id ="forrepair_que" class="btn btn-primary testings">For Repair</button>
+                              </div>
+                              <div class="btn-group col-lg-2 col-md-12 col-xs-12" role="group">
+                                        <button type="button" id ="releasing_que" class="btn btn-primary testings">Releasing</button>
+                              </div>
+                                      
+                              <div class="btn-group col-lg-2 col-md-12 col-xs-12" role="group">
+                                        <button type="button" id ="appointment_que" class="btn btn-primary testings">Appointment</button>
+                              </div>
+                                      
+                              <div class="btn-group col-lg-2 col-md-12 col-xs-12" role="group">
+                                        <button type="button" id ="inquiry_que" class="btn btn-primary testings">Inquiry</button>
+                              </div>
+                 
+                 						<input type="hidden" class="selected_que" value="ALL">
+                          
+                   
+                 	  </div><!---row-->   
 </div> <!---container-fluid-->
-   
-
-
-			<!--		<div class="refreshbuts"></div>
-
-					<div class="pmclogso"></div>
-
-
-
-</div> <!------ clientfield closure --->
 
 
 
